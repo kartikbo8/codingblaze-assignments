9266cfb5cb55aee88cb92fea08e9e7b6c4225f45
kartikbo8 <109646601+kartikbo8@users.noreply.github.com>
2022-08-03 20:38:10 +0530

    Add files via upload

---
+++ Assignment6.js
@@ -0,0 +1,35 @@
+let flat = [
+    {
+        flatNumber: 1,
+        tenantName: "Ravish",
+        totalRent: 20000,
+        rentPaid: 18000,
+    },
+    {
+        flatNumber: 2,
+        tenantName: "Suraj",
+        totalRent: 15000,
+        rentPaid: 15000,
+    },
+    {
+        flatNumber: 3,
+        tenantName: "Akash",
+        totalRent: 25000,
+        rentPaid: 25000,
+    },
+    {
+        flatNumber: 4,
+        tenantName: "Sanjay",
+        totalRent: 18000,
+        rentPaid: 15000,
+    },
+    {
+        flatNumber: 5,
+        tenantName: "Akshay",
+        totalRent: 24000,
+        rentPaid: 24000,
+    },
+];
+
+let flatNumbers = arr.filter(totalRent);
+flatNumbers.name.paid > 20000;
